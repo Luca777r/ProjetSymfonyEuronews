1f77fb79d01d9c421fc3216dae05910333cba02f
Lucius <lucadelrosso.pro@gmail.com>
2022-02-17 22:31:32 +0100

    connexion à l'API marvel

---
+++ src/Controller/MarvelController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MarvelController extends AbstractController
+{
+    #[Route('/monappli.com/comics/avengers', name: 'marvel')]
+    public function index(): Response
+    {
+        $ts = time();
+        $private = "7ccb4de3a15ad51661809fde5ca3743ebaac6956";
+        $public = "58c3607be4b218a65242d720871dee86";
+        $hash = md5($ts . $private . $public);
+        $httpClient = HttpClient::create([], 6, 10);
+        $response = $httpClient->request(
+            'GET',
+            'https://gateway.marvel.com:443/v1/public/comics?ts=' . $ts . '&apikey=58c3607be4b218a65242d720871dee86&hash=' . $hash,
+        );
+
+        $statusCode = $response->getStatusCode();
+        $content = $response->getContent();
+
+        return $this->render('marvel/index.html.twig', [
+            'controller_name' => 'MarvelController',
+            "data" => $content
+        ]);
+    }
+}
