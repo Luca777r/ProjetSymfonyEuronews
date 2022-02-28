28a6f8313438b7b2e30ef9e3b755bee784a4971b
Lucius <lucadelrosso.pro@gmail.com>
2022-02-28 16:53:29 +0100

    characters v1

--- src/Controller/MarvelController.php
+++ src/Controller/MarvelController.php
@@ -29,7 +29,30 @@ class MarvelController extends AbstractController
         // dd($data);
 
         return $this->render('marvel/index.html.twig', [
-            'controller_name' => 'MarvelController',
+            "response" => $data
+        ]);
+    }
+
+    #[Route('/monappli.com/comics/avengers/characters/{id}', name: 'marvel_characters')]
+    public function characters($id): Response
+    {
+        $ts = time();
+        $private = "7ccb4de3a15ad51661809fde5ca3743ebaac6956";
+        $public = "58c3607be4b218a65242d720871dee86";
+        $hash = md5($ts . $private . $public);
+        $httpClient = HttpClient::create([], 6, 10);
+        $response = $httpClient->request(
+            'GET',
+            'https://gateway.marvel.com:443/v1/public/comics/' . $id . '/characters?apikey=' . $public .
+                '&ts=' . $ts . '&hash=' . $hash,
+        );
+
+        $statusCode = $response->getStatusCode();
+        $content = $response->getContent();
+        $data = json_decode($content);
+        // dd($data);
+
+        return $this->render('marvel/characters.html.twig', [
             "response" => $data
         ]);
     }
